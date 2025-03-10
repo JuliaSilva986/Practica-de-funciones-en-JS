887c065bb61550515dd8bfd5463864a018b65bd8
Julia Silva <jsilva27@ucol.mx>
2025-03-09 22:16:09 -0600

    correcion de archivo funciones.js

--- funciones.js
+++ funciones.js
@@ -7,15 +7,15 @@ Instrucciones para resolver el problema:
 Define una función `agregarLibro(titulo)`, que añada un libro a un array llamado `librosLeidos`.
 Define una función `mostrarLibrosLeidos()`, que imprima todos los libros que has leído. */
 
-let arrayLibros=[];
+let librosLeidos=[];
 
 function agregarLibro(titulo){
-    return arrayLibros.push(titulo);
+    return librosLeidos.push(titulo);
 }
 
 function mostrarLibrosLeidos(){
-    for (let i=0; i<arrayLibros.length; i++){
-        console.log(arrayLibros[i]);
+    for (let i=0; i<librosLeidos.length; i++){
+        console.log(librosLeidos[i]);
     }
 }
 
